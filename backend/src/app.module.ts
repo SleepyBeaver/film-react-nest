2c783ee2b17a3a8ce5bbabf176673290b61596e0
Kuznetsov Dmitry <dima.mr-smith@yandex.ru>
2025-08-11 20:15:45 +0300

    Исправлена раздача статики

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,7 +14,6 @@ import * as path from 'path';
     OrderModule,
     ServeStaticModule.forRoot({
       rootPath: path.join(__dirname, '..', 'public'),
-      serveRoot: '/content/afisha',
     }),
   ],
 })
